1260379fd87229c53afc56d41cb91aa0ac9b53ef
Volodymyr Kozeko <vova.kozenko5@gmail.com>
2024-03-18 07:20:56 +0100

    Remove markers through users map

--- EmptyParking/wwwroot/js/Map.js
+++ EmptyParking/wwwroot/js/Map.js
@@ -1,10 +1,12 @@
+var map;
+var markers = [];
 function setUpMap(data) {
     let mapOptions = {
         center: [50.0299, 19.9099],
         zoom: 13
     };
 
-    var map = L.map('map', mapOptions);
+    map = L.map('map', mapOptions);
 
     let layer = new L.TileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png')
     map.addLayer(layer);
@@ -17,9 +19,33 @@ function setUpMap(data) {
 
     data.forEach((item) => {
         if (item.Longitude && item.Latitude) {
+            var popupContent =
+                '<p>Some Information</p></br>' +
+                '<p>test</p></br>' +
+                '<button onclick="clearMarker(' + item.Id + ')">Clear Marker</button>';
+
             let marker = new L.Marker([item.Longitude, item.Latitude]);
-            marker.addTo(map);
+            marker._id = item.Id;
+            marker.bindPopup(popupContent, {
+                closeButton: false
+            });
+            map.addLayer(marker);
+            markers.push(marker);
         }
     });
 
 };
+
+function clearMarker(id) {
+    var index = -1;
+    markers.forEach(function (marker) {
+        if (marker._id == id) {
+            map.removeLayer(marker);
+            index = markers.indexOf(marker);
+        }
+    });
+
+    if (index > -1) { 
+        markers.splice(index, 1);
+    }
+}
