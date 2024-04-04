25803d5cf54b362fbe40005a500f1ca31bf16ab6
Volodymyr Kozeko <vova.kozenko5@gmail.com>
2024-04-04 21:07:14 +0200

    Added signalR at two separate projects -  server, client

---
+++ SignalRClient/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var connection = new HubConnectionBuilder()
+            .WithUrl("http://localhost:5000/chatHub")
+            .Build();
+
+        connection.StartAsync().Wait();
+        connection.InvokeCoreAsync("SendMessage", args: new[] { "Vova", "Hello" });
+
+        connection.On("ReceiveMessage", (string userName, string message) =>
+        {
+            Console.WriteLine(userName + ":" + message);
+        });
+
+        Console.ReadLine();
+    }
+}
---
+++ SignalRServer/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+
+namespace SignalRServer
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        private static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+            .UseStartup<Startup>();
+    }
+}
