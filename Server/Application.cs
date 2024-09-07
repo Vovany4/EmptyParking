cd241b83bdf23d83eddbd8953b83db8cce43db79
Volodymyr Kozeko <vova.kozenko5@gmail.com>
2024-09-08 00:58:14 +0200

    Corrected - agregate

--- Server/Application.cs
+++ Server/Application.cs
@@ -28,30 +28,86 @@ namespace Server
             using var channel = cnn.CreateModel();
 
             string queueName = "DemoQueue";
-            
-            channel.QueueDeclare(queueName, false, false, false);
-            channel.BasicQos(0, 1, false);
+            var spotAggregation = new Dictionary<int, Spot>();
+            int timeIntervalToWaitSec = 30;
+            uint maxMessagesToConsume = 100;
 
-            var consumer = new EventingBasicConsumer(channel);
-            consumer.Received += async (sender, args) =>
+            while (true)
             {
-                await ConsumerLogicAsync(channel, args);
-            };
+                uint messagesToConsume = 0;
+                var waitTillMessagesConsuming = true;
+                var consumerTag = string.Empty;
+                var startTime = DateTime.Now;
+                var aggregatedMessages = 0;
 
-            var consumerTag = channel.BasicConsume(queueName, false, consumer);
+                while ((DateTime.Now - startTime).TotalSeconds < timeIntervalToWaitSec)
+                {
+                    var queueDeclareOk = channel.QueueDeclare(queueName, false, false, false);
+                    messagesToConsume = queueDeclareOk.MessageCount;
 
-            Console.ReadLine();
+                    Console.WriteLine($"Current message count in the queue '{queueName}': {messagesToConsume}");
 
-            channel.BasicCancel(consumerTag);
+                    if (messagesToConsume >= maxMessagesToConsume)
+                    {
+                        Console.WriteLine($"Message count is {messagesToConsume} or more, starting to consume messages.");
+                        break;
+                    }
+
+                    // Wait for a short period before checking again
+                    Thread.Sleep(1000);
+                }
+
+                if (messagesToConsume > 0)
+                {
+                    channel.BasicQos(0, (ushort)messagesToConsume, false);
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += async (sender, args) =>
+                    {
+                        if (aggregatedMessages + 1 >= messagesToConsume)
+                        {
+                            channel.BasicCancel(consumerTag); // Stop consuming new messages until process collected
+                        }
+
+                        var body = args.Body.ToArray();
+                        string message = Encoding.UTF8.GetString(body);
+                        var spot = JsonConvert.DeserializeObject<Spot>(message) ?? throw new Exception("spot is null");
+
+                        if (aggregatedMessages < messagesToConsume)
+                        {
+                            spotAggregation[spot.Id] = spot;
+                            aggregatedMessages++;
+                        }
+
+                        if (aggregatedMessages >= messagesToConsume)
+                        {
+                            foreach (var spotElem in spotAggregation)
+                            {
+                                await ConsumerLogicAsync(spotElem.Value);
+                            }
+                            channel.BasicAck(args.DeliveryTag, true); // Acknowledge aggregated messages
+                            spotAggregation.Clear();
+                            waitTillMessagesConsuming = false;
+                        }
+                    };
+
+                    consumerTag = channel.BasicConsume(queueName, false, consumer);
+
+                    WaitTillMessagesConsuming(ref waitTillMessagesConsuming);
+                }
+            }
         }
 
-        private async Task ConsumerLogicAsync(IModel channel, BasicDeliverEventArgs args)
+        private static void WaitTillMessagesConsuming(ref bool waitTillMessagesConsuming)
         {
-            var body = args.Body.ToArray();
-            string message = Encoding.UTF8.GetString(body);
+            while (waitTillMessagesConsuming)
+            {
+                Thread.Sleep(100); // Small delay to avoid busy waiting
+            }
+        }
 
-            var spot = JsonConvert.DeserializeObject<Spot>(message);
-
+        private async Task ConsumerLogicAsync(Spot spot)
+        {
             if (spot is not null)
             {
                 Console.WriteLine($"Message Received:");
@@ -80,8 +136,6 @@ namespace Server
             {
                 Console.WriteLine($"Spot is null");
             }
-
-            channel.BasicAck(args.DeliveryTag, false);
         }
     }
 }
